2179266580445ef36d4ffef13c146542abdb7059
Harsh kolambkar <105577253+Harshkolambkar@users.noreply.github.com>
2023-10-29 19:36:20 +0530

    update in checkout

--- checkout.js
+++ checkout.js
@@ -48,3 +48,27 @@ function updateTotalPrice() {
 
 // Call the function to initially display the total price
 updateTotalPrice();
+
+// JavaScript code to handle the "Buy Now" button click event
+document.getElementById("buyNow").addEventListener("click", function () {
+    // Check if the user is logged in (you need to implement your own logic for this)
+    var isLoggedIn = false; // Set this to true if the user is logged in
+
+    // If the user is not logged in, display an alert with a message and a button
+    if (!isLoggedIn) {
+        var alertMessage = "Please login before buying.";
+        var alertButton = "Go to Registration Page";
+
+        // Create an alert with a message and a button
+        var confirmation = confirm(alertMessage + "\n\nPress OK to go to the registration page.");
+
+        // If the user confirms, redirect to the registration page
+        if (confirmation) {
+            // Redirect the user to the registration page
+            window.location.href = "registration_page.html";
+        }
+    } else {
+        // If the user is logged in, proceed with the purchase logic
+        // Add your purchase code here
+    }
+});
